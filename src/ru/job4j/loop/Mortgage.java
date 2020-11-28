8d7e4bfbf39962e17365eabf1e990a11b2a9e791
Kirill Vasin <vukivur@gmail.com>
2020-11-28 13:16:26 +0300

    5.7. Ипотека [#412350]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        int debt;
         while (amount > 0) {
-            debt = amount;
-            amount = (int) (amount * percent) + debt - salary;
+            amount = (int) (amount * percent) + amount - salary;
             year++;
         }
         return year;
