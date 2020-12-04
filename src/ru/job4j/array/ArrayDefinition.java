ee56f4436e196e3f7e5e5f6a00505d3161768686
Kirill Vasin <vukivur@gmail.com>
2020-12-05 00:02:28 +0300

    1. Чтение кода и базовые ошибки [#412430]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -6,16 +6,13 @@ public class ArrayDefinition {
         String[] surnames = new String[100500];
         float[] prices = new float[40];
         String[] names = new String[4];
-
         names[0] = "Chaffee";
         names[1] = "Patton";
         names[2] = "Heavy";
         names[3] = "Hellcat";
-
         System.out.println("Размер массива 'ages' равен: " + ages.length);
         System.out.println("Размер массива 'surnames' равен: " + surnames.length);
         System.out.println("Размер массива 'prices' равен: " + prices.length);
-
         System.out.println("\nArray 'names':\n");
         for (String index : names) {
             System.out.println(index);
