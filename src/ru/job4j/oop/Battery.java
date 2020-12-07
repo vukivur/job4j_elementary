49bc13147fcb4cabebd2d5b4e92b46d4f5d931e5
Kirill Vasin <vukivur@gmail.com>
2020-12-07 08:40:06 +0300

    1.7. Состояние объекта [#412457]

--- src/ru/job4j/oop/Battery.java
+++ src/ru/job4j/oop/Battery.java
@@ -8,8 +8,8 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        this.load += another.load;
-        another.load = 0;
+        another.load += this.load;
+        this.load = 0;
     }
 
     public static void main(String[] args) {
